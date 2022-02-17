19e8ec06fe0a78a8449967037c21905d576c999b
Jami Couch <jami.couch@twinsunsolutions.com>
2022-02-17 13:43:13 -0600

    [main] Update Facebook SDK to 12.3.1 for iOS

--- ios/Classes/SwiftFlutterFacebookAppLinksPlugin.swift
+++ ios/Classes/SwiftFlutterFacebookAppLinksPlugin.swift
@@ -1,10 +1,9 @@
+import FBSDKCoreKit
 import Flutter
 import UIKit
-import FBSDKCoreKit
 
 public class SwiftFlutterFacebookAppLinksPlugin: NSObject, FlutterPlugin {
-
-  //fileprivate var resulter: FlutterResult? = nil
+  // fileprivate var resulter: FlutterResult? = nil
 
   public static func register(with registrar: FlutterPluginRegistrar) {
     let channel = FlutterMethodChannel(name: "plugins.remedia.it/flutter_facebook_app_links", binaryMessenger: registrar.messenger())
@@ -12,73 +11,64 @@ public class SwiftFlutterFacebookAppLinksPlugin: NSObject, FlutterPlugin {
 
     // Get user consent
     print("FB APP LINK registering plugin")
-    ApplicationDelegate.initializeSDK(nil)
+    ApplicationDelegate.shared.initializeSDK()
 
     registrar.addMethodCallDelegate(instance, channel: channel)
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-        case "consentProvided":
-            Settings.isAutoLogAppEventsEnabled = true
-            ApplicationDelegate.initializeSDK(nil)
-            result(nil)
-            break
-        case "consentRevoked":
-            Settings.isAutoLogAppEventsEnabled = false
-            ApplicationDelegate.initializeSDK(nil)
-            result(nil)
-            break
-        case "getPlatformVersion":
-            handleGetPlatformVersion(call, result: result)
-            break
-        case "initFBLinks":
-            print("FB APP LINK launched")
-            handleFBAppLinks(call, result: result)
-            break
-        default:
-            result(FlutterMethodNotImplemented)
-        }
+    case "consentProvided":
+      Settings.shared.isAutoLogAppEventsEnabled = true
+      ApplicationDelegate.shared.initializeSDK()
+      result(nil)
+    case "consentRevoked":
+      Settings.shared.isAutoLogAppEventsEnabled = false
+      ApplicationDelegate.shared.initializeSDK()
+      result(nil)
+    case "getPlatformVersion":
+      handleGetPlatformVersion(call, result: result)
+    case "initFBLinks":
+      print("FB APP LINK launched")
+      handleFBAppLinks(call, result: result)
+    default:
+      result(FlutterMethodNotImplemented)
+    }
   }
 
-  private func handleGetPlatformVersion(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+  private func handleGetPlatformVersion(_: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
 
-  private func handleFBAppLinks(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+  private func handleFBAppLinks(_: FlutterMethodCall, result: @escaping FlutterResult) {
     print("FB APP LINKS Starting ")
 
-    AppLinkUtility.fetchDeferredAppLink { (url, error) in
-        if let error = error {
-          print("Received error while fetching deferred app link %@", error)
-          result(nil);
+    AppLinkUtility.fetchDeferredAppLink { url, error in
+      if let error = error {
+        print("Received error while fetching deferred app link %@", error)
+        result(nil)
+      }
+
+      if let url = url {
+        print("FB APP LINKS getting url: ", String(url.absoluteString))
+
+        var mapData: [String: String?] = ["deeplink": url.absoluteString, "promotionalCode": nil]
+
+        if let code = AppLinkUtility.appInvitePromotionCode(from: url) {
+          print("promotional code " + String(code))
+          mapData["promotionalCode"] = code
+        } else { // nil
         }
 
-        if let url = url {
-          print("FB APP LINKS getting url: ", String(url.absoluteString) )
-
-          var mapData : [String: String?] = ["deeplink": url.absoluteString, "promotionalCode": nil]
-
-          if let code = AppLinkUtility.appInvitePromotionCode(from: url) {
-            print("promotional code " + String(code))
-            mapData["promotionalCode"] = code
-          } else { // nil
-
-          }
-
-          if #available(iOS 10, *) {
-            result(mapData)
-          } else {
-            result(mapData)
-          }
-        }else{
-          // no deep link received
-          result(nil)
+        if #available(iOS 10, *) {
+          result(mapData)
+        } else {
+          result(mapData)
         }
+      } else {
+        // no deep link received
+        result(nil)
+      }
     }
-
-
   }
-
-
 }
